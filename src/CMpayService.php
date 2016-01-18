a9070f5bf7693957b848454b478a8f26d66097d1
Chilion Snoek <chilionsnoek@gmail.com>
2016-01-18 15:36:14 +0100

    $parameters can't be null

--- src/CMpayService.php
+++ src/CMpayService.php
@@ -55,7 +55,7 @@ class CmPayService
      *
      * @return bool|string
      */
-    public static function getTransactionUrl($amount, $method, $option, array $parameters = null)
+    public static function getTransactionUrl($amount, $method, $option, $parameters)
     {
 
         // Standard data
